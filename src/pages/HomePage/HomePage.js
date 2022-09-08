4be797b7f7ab65835fbf244117eb0cdb14b28006
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2022-09-08 18:06:48 +0530

    1 file modified

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
 import './HomePage.scss';
 
+const iState = {
+  isLoading: false,
+  data: [],
+  error: null,
+};
+
+const Actions_Type = {
+  Fetch_Start: 'Fetch_Start',
+  Fetch_Success: 'Fetch_Success',
+  Fetch_Error: 'Fetch_Error',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case Actions_Type.Fetch_Start:
+      return { ...state, isLoading: true };
+    case Actions_Type.Fetch_Success:
+      return { ...state, isLoading: false, data: action.payload };
+    case Actions_Type.Fetch_Error:
+      return { ...state, isLoading: false, error: action.payload };
+    default:
+      return { state };
+  }
+};
+
 const HomePage = () => {
+  const [state, dispatch] = useReducer(reducer, iState);
+
+  const fetchContentHandler = () => {
+    dispatch({
+      type: Actions_Type.Fetch_Start,
+    });
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        dispatch({
+          type: Actions_Type.Fetch_Success,
+          payload: data,
+        });
+      })
+      .catch((err) => {
+        dispatch({
+          type: Actions_Type.Fetch_Error,
+          payload: err,
+        });
+      });
+  };
+
+  console.log(state);
+
   return (
     <Container>
       <Row>
         <Col>
-          <Button>Fetch Content</Button>
+          <Button onClick={fetchContentHandler}>Fetch Content</Button>
         </Col>
       </Row>
     </Container>
