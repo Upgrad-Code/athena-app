7277282656f2f81057cbc4497d13528a174d27bf
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2022-09-09 11:43:34 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
